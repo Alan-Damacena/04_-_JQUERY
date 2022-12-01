75a145e8139be88484f551074df95005f5c1cbc4
Alan Damacena <alanfmdamacena@outlook.com>
2022-12-01 11:19:36 -0300

    js

--- 02_-_PRIMEIROS_CODIGOS/script.js
+++ 02_-_PRIMEIROS_CODIGOS/script.js
@@ -1,1 +1,7 @@
-let btn_somarjs = document.getElementById("btn_somarjs").addEventListener("click",)
+//USANDO JAVASCRIPT
+let btn_somarjs = document.getElementById("btn_somarjs").addEventListener("click",()=>{
+    let res = document.getElementById("res").value = parseInt(document.getElementById("v1").value) + parseInt(document.getElementById("v2").value);
+})
+
+//USANDO JQUERY
+$("#")
