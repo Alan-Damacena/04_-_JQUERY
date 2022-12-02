6dd49260e5ca4af1af631ac5930f3d6c5dbcc842
Alan Damacena <alanfmdamacena@outlook.com>
2022-12-02 14:15:57 -0300

    jq

--- 02_-_PRIMEIROS_CODIGOS/script.js
+++ 02_-_PRIMEIROS_CODIGOS/script.js
@@ -3,5 +3,10 @@ let btn_somarjs = document.getElementById("btn_somarjs").addEventListener("click
     let res = document.getElementById("res").value = parseInt(document.getElementById("v1").value) + parseInt(document.getElementById("v2").value);
 })
 
-//USANDO JQUERY
-$("#")
+//USANDO JQUERY ($ = apontar para ...)
+$("#btn_somarjq").click(()=>{
+    let v1 = parseInt($("#v1").val());
+    let v2 = parseInt($("#v2").val());
+    let soma = v1 + v2;
+    $("#res").val(soma);
+})
